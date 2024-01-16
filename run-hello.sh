17978cb997d6f050e5cad920019501c1de1c221a
vinita12d <156658444+vinita12d@users.noreply.github.com>
2024-01-16 17:34:01 +0530

    Update run-hello.sh
    updated

--- run-hello.sh
+++ run-hello.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-
+ 
 # Source the prancer-lib shell script to call all pr-* library functions.
 source shlib/prancer-lib.sh
-
+ 
 # generate a hello alert, pass the target like: "https://brokencrystals.com/"
-output=`pr_hello_world_alert "$1"`
-
-# Check the output
-if [ $? -ne 0 ];  then
-  echo "Failed to generate alert!...."
+# output=`pr_hello_world_alert "$1"`
+#
+# # Capture the URL passed as an argument
+url="$1"
+ 
+# Use curl to fetch data from the modified URL and store the HTTP response code in a variable
+code=$(curl --write-out '%{http_code}' --silent --output /dev/null "$url")
+ 
+if [ $code -eq 200 ]; then
+  output=`pr_hello_world_alert "$1"`
+  echo $output
 else
-  echo "$output"
+  echo "Site is not existing.... valid"
 fi
-
-exit 0
