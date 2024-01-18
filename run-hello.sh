3a0ff1c67f3ea6501edce229c160d0e7c8db7f19
vinita12d <156658444+vinita12d@users.noreply.github.com>
2024-01-18 14:38:41 +0530

    Update run-hello.sh
    Update

--- run-hello.sh
+++ run-hello.sh
@@ -3,67 +3,89 @@
 # Source the prancer-lib shell script to call all pr-* library functions.
 source shlib/prancer-lib.sh
 
-# Check if a URL is provided
-if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 <target_url>"
+# Checks for correct usage/syntax
+if [ "$#" -lt 1 ]; then
+  echo "Usage: $0 <Target URL/IP> [Port Range]"
+  echo "Default Port Range: 22-443"
   exit 1
 fi
 
-# The target URL
-URL=$1
+# Assigned command line arguments
+fqdn=`python3 shlib/prancer_fqdn.py $1`
+# echo $fqdn
+TARGET="$fqdn"
 
-# Using curl to get the HTTP response code
-RESPONSE=$(curl -o /dev/null -s -w "%{http_code}\n" "$URL")
+PORT_RANGE="22-443"
+if [ "$#" -gt 1 ]; then
+  PORT_RANGE="$2"
+fi
 
-# Check if the response code is 200
-if [ "$RESPONSE" -ge 200 ] && [ "$RESPONSE" -le 299 ]; then
- title="Requested site $URL is live. Status code $RESPONSE"
- severity="Low"
- desc="The returned status code is between 200 and 226 implying the site is live and healthy."
- alertid="200"
- solution="No action needed unless the target URL is not supposed to be live, in which case it's advisable to take the website down until it's ready to go live."
- cweid="199"
- wascid=
- reference="https://developer.mozilla.org/en-US/docs/Web/HTTP/Status"
+# Setting the array name
+OPEN_PORTS=()
 
- msg_out1_file=`pr_get_message_json "$url" "" "" "$modified_url" "$r"`
- # cat msg_out1_file
+#Function for netcat scan
+check_port() {
+  if nc -z -w 1 $1 $2; then
+     # Add the open port to the array
+     OPEN_PORTS+=($2)
+  fi
+}
 
- al_out_file=`pr_get_alert_json "$alertid" "$title" "$severity" "$desc" "1" "$solution" "$reference" "$cweid" "$wascid" ""`
+# Splitting the port range into 'start' and 'end'
+IFS="-" read START_PORT END_PORT <<< "$PORT_RANGE"
+
+# Scanning each port in the range
+for ((port=START_PORT; port<=END_PORT; port++)); do
+  check_port $TARGET $port
+done
+
+# Check if any open ports were found
+if [ ${#OPEN_PORTS[@]} -ne 0 ]; then
+# echo "Open ports on $TARGET found: ${OPEN_PORTS[*]}"
+  OPEN_PORTS_FOUND=true
+     title="Open ports found on $TARGET found: ${OPEN_PORTS[*]}"
+     severity="High"
+     desc="Open or exposed ports increase the potential attack surface and could leave you vulnerable to compromise."
+     alertid="202"
+     solution=""
+     cweid=1125
+     wascid=15
+     reference="https://cwe.mitre.org/data/definitions/1125.html"
+
+     msg_out1_file=`pr_get_message_json "$TARGET" "" "" "$modified_url" "$r"`
+    # cat msg_out1_file
+     al_out_file=`pr_get_alert_json "$alertid" "$title" "$severity" "$desc" "1" "$solution" "$reference" "$cweid" "$wascid" ""`
  
-pr_update_json "$al_out_file" "$msg_out1_file" "instances"
+    pr_update_json "$al_out_file" "$msg_out1_file" "instances"
 
- # Get an temporary site json file
-  site_out_file=`pr_site_file $1`
-  pr_update_json "$site_out_file" "$al_out_file" "alerts"
+  # Get an temporary site json file
+    site_out_file=`pr_site_file $1`
+    pr_update_json "$site_out_file" "$al_out_file" "alerts"
   # echo "SITE: $site_out_file"
-
   # Get an temporary result json file
-  result_out_file=`pr_result_file $1`
-  pr_update_json "$result_out_file" "$site_out_file" "site"
+    result_out_file=`pr_result_file $1`
+    pr_update_json "$result_out_file" "$site_out_file" "site"
   # echo "result: $result_out_file"
-
   # Get an temporary spider json file
   # spider_out_file=`pr_spider_file $1`
   # echo "spider: $spider_out_file"
-
   # Build the alert output json file
-  out_file=`pr_output_file`
+    out_file=`pr_output_file`
   # echo "OUTPUT: $out_file"
-  pr_update_json "$out_file" "$result_out_file" "Result"
+    pr_update_json "$out_file" "$result_out_file" "Result"
   # pr_update_json "$out_file" "$spider_out_file" "Spider"
-
 # print_and_write "$out_file" "$site_out_file" "site"
 
- # Check the output
- if [ $? -ne 0 ]; then
- echo "Failed to generate alert!...."
- else
- cat $out_file
- fi
-else
- echo "The site $URL may not be live. Status code $RESPONSE"
- exit 1
+# Check the output
+  if [ $? -ne 0 ]; then
+  echo "Failed to generate alert!...."
+  else
+  cat $out_file
+  fi
+  # Handle the case when no ports are found
+  else
+  echo "No open ports found on $TARGET"
+exit 1
 fi
 
 exit 0
